498ffcf30a2a8375e68aef5caf86b39fa75577d0
Dave Whiffen <david.whiffen@ryerson.ca>
2019-10-22 13:40:18 -0400

    New Map and Ball Mechanics

---
+++ Assets/scripts/followPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public GameObject player;        
+
+
+    private Vector3 offset;            
+
+    
+    void Start()
+    {
+       
+        offset = transform.position - player.transform.position;
+    }
+
+   
+    void LateUpdate()
+    {
+        
+        transform.position = player.transform.position + offset;
+        transform.localScale = player.transform.localScale;
+        transform.rotation = player.transform.rotation;
+    }
+}
